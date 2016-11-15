1194324d29bb0e3531fa9eaf6dcd93109e4ddf1e
Kevin Ding <Kevin Ding>
2016-11-14 22:15:54 -0500

    added jaws db heroku deployment connecction config

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,15 @@
 var Sequelize = require('sequelize'), connection;
 
 if(process.env.JAWSDB_URL) {
-
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log('jaws_db_loaded');
 } else {
-
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  });
 }
 
 
